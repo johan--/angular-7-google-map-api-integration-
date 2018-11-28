d7cc61764f8167e79c71538720b52df5c75747af
rajneeshksharma <rajneeshksharmaoffice@gmail.com>
2018-11-28 12:28:58 +0530

    angular materail and pwa function

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GooglePlaceModule } from "ngx-google-places-autocomplete";
-import {AgmCoreModule } from '@agm/core';
-
-
+import { AgmCoreModule } from '@agm/core';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule, MatInputModule } from '@angular/material';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -17,9 +22,16 @@ import {AgmCoreModule } from '@agm/core';
     AppRoutingModule,
     GooglePlaceModule,
     AgmCoreModule.forRoot({
-       apiKey:'************-_*****', //Your api key here
-        libraries: ["places"]
-    })
+      apiKey: 'Api-key-here', //Your api key here
+      libraries: ["places"]
+    }),
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
